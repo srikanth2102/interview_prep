622bf496963546410cb39292bcb63842474d5d6f
1192928 <sri212002kanth@gmail.com>
2024-12-26 17:55:38 +0530

    bubble sort

---
+++ Sorting Algorithms/bubble_sort.py
@@ -0,0 +1,21 @@
+#bubble sort
+
+'''
+
+compare the adjacent elements and then swap if right is greater
+
+'''
+
+arr = [23,22,324,212,353,12434,243,1,5,9]
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        try:
+            if(arr[j+1]<arr[j]):
+                temp = arr[j+1]
+                arr[j+1]=arr[j]
+                arr[j]=temp
+        except IndexError:
+            break;
+    print(arr)
+print(arr)
